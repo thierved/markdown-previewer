f4686d6a7262966e79a906639328876aa37e047b
Thierno Sadou Diallo <thierved01@gmail.com>
2018-06-10 15:52:19 +0000

    styled the previewer and the editor

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import marked from 'marked';
 
-const App = () => {
-  return (
-    <div>
-     
-    </div>
-  );
+
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false,
+});
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markdown : ''
+    }
+  }
+
+  render() {
+    return (
+      <div className='container'>        
+        <div className='screen'>
+        <h4>Editor</h4>
+          <textarea id='editor' onInput={(e) => this.setState({markdown : e.target.value}) }/>
+        </div>
+
+        <div className='screen'>
+          <h4>Preview</h4>
+          <div id='preview'>
+            {marked(this.state.markdown)}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
 }
 
 export default App;
