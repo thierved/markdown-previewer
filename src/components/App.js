77f922d84a0094177eca26f1a3411fff5dbe2017
Thierno Sadou Diallo <thierved01@gmail.com>
2018-06-12 18:16:01 +0000

    markdown works

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,10 @@ class App extends Component {
     super(props);
 
     this.state = {
-      input : ""
+      input : this.defaultMark()
     }
   }
+
 
   convetToMarkdown() {
     let md = marked(this.state.input, {sanitize: true});
@@ -22,7 +23,7 @@ class App extends Component {
       <div className='container'>        
         <div className='screen'>
         <h4>Editor</h4>
-          <textarea id='editor' onInput={(e) => this.setState({input : e.target.value})}/>
+          <textarea id='editor' value={this.state.input} onChange={(e) => this.setState({input : e.target.value})}/>
         </div>
 
         <div className='screen'>
@@ -31,6 +32,55 @@ class App extends Component {
         </div>
       </div>
     )
+  } 
+
+  defaultMark() {
+    return `# Welcome to my React Markdown Previewer!
+
+    ## This is a sub-heading...
+    ### And here's some other cool stuff:
+      
+    Heres some code, \`<div></div>\`, between 2 backticks.
+    
+    \`\`\`
+    // this is multi-line code:
+    
+    function anotherExample(firstLine, lastLine) {
+      if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+        return multiLineCode;
+      }
+    }
+    \`\`\`
+      
+    You can also make text **bold**... whoa!
+    Or _italic_.
+    Or... wait for it... **_both!_**
+    And feel free to go crazy ~~crossing stuff out~~.
+    
+    There's also [links](https://www.freecodecamp.com), and
+    > Block Quotes!
+    
+    And if you want to get really crazy, even tables:
+    
+    Wild Header | Crazy Header | Another Header?
+    ------------ | ------------- | ------------- 
+    Your content can | be here, and it | can be here....
+    And here. | Okay. | I think we get it.
+    
+    - And of course there are lists.
+      - Some are bulleted.
+         - With different indentation levels.
+            - That look like this.
+    
+    
+    1. And there are numbererd lists too.
+    1. Use just 1s if you want! 
+    1. But the list goes on...
+    - Even if you use dashes or asterisks.
+    * And last but not least, let's not forget embedded images:
+    
+    ![React Logo w/ Text](https://goo.gl/Umyytc)
+    `
   }
 
 }
