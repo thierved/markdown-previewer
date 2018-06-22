5530b707716fb2df5722d2ab0a0b7b1b4443b070
Thierno Sadou Diallo <thierved01@gmail.com>
2018-06-22 19:01:29 +0000

    passed some test

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,29 @@ import marked from 'marked';
 
 
 
+const renderer = new marked.Renderer();
+renderer.link = function(href, title, text) {
+  const link = marked.Renderer.prototype.link.call(this, href, title, text);
+  return link.replace("<a","<a target='_blank' ");
+};
+
+marked.setOptions({
+  breaks: true,
+  renderer: renderer,
+});
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      input : this.defaultMark()
+      input : this.defaultMarkdown
     }
   }
 
 
   convetToMarkdown() {
-    let md = marked(this.state.input, {sanitize: true});
+    let md = marked(this.state.input);
     return {__html : md};
   }
 
@@ -33,56 +44,55 @@ class App extends Component {
       </div>
     )
   } 
+ defaultMarkdown = 
+  `# Welcome to my React Markdown Previewer!
 
-  defaultMark() {
-    return `# Welcome to my React Markdown Previewer!
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+    
+  Heres some code, \`<div></div>\`, between 2 backticks.
 
-    ## This is a sub-heading...
-    ### And here's some other cool stuff:
-      
-    Heres some code, \`<div></div>\`, between 2 backticks.
-    
-    \`\`\`
-    // this is multi-line code:
-    
-    function anotherExample(firstLine, lastLine) {
-      if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-        return multiLineCode;
-      }
+  \`\`\`
+  // this is multi-line code:
+
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
     }
-    \`\`\`
-      
-    You can also make text **bold**... whoa!
-    Or _italic_.
-    Or... wait for it... **_both!_**
-    And feel free to go crazy ~~crossing stuff out~~.
-    
-    There's also [links](https://www.freecodecamp.com), and
-    > Block Quotes!
-    
-    And if you want to get really crazy, even tables:
-    
-    Wild Header | Crazy Header | Another Header?
-    ------------ | ------------- | ------------- 
-    Your content can | be here, and it | can be here....
-    And here. | Okay. | I think we get it.
-    
-    - And of course there are lists.
-      - Some are bulleted.
-         - With different indentation levels.
-            - That look like this.
-    
-    
-    1. And there are numbererd lists too.
-    1. Use just 1s if you want! 
-    1. But the list goes on...
-    - Even if you use dashes or asterisks.
-    * And last but not least, let's not forget embedded images:
-    
-    ![React Logo w/ Text](https://goo.gl/Umyytc)
-    `
   }
+  \`\`\`
+    
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
 
+  There's also [links](https://www.freecodecamp.com), and
+   > Block Quotes!
+
+  And if you want to get really crazy, even tables:
+
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | ------------- 
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+
+  - And of course there are lists.
+    - Some are bulleted.
+        - With different indentation levels.
+          - That look like this.
+
+
+  1. And there are numbererd lists too.
+  1. Use just 1s if you want! 
+  1. But the list goes on...
+  - Even if you use dashes or asterisks.
+  * And last but not least, let's not forget embedded images:
+
+  ![React Logo w/ Text](https://goo.gl/Umyytc)
+  `;
 }
+
+
 
 export default App;
